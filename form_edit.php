6c51b9b949c252037b5d522b5b505d83ecf8dee1
Thainá Goes Sampaio <91396294+thainasampaio@users.noreply.github.com>
2021-10-13 14:43:39 -0300

    Update form_edit.php

--- form_edit.php
+++ form_edit.php
@@ -5,7 +5,7 @@
 	  E-mail:<br/> 
 	  <input type="email" name="email" placeholder="Qual seu e-mail?"><br/><br/>
 	  Instituição de ensino:<br/> 
-	  <input type="text" name="escola" placeholder="Qual sua cidade?"><br/><br/>
+	  <input type="text" name="escola" placeholder="Qual sua escola?"><br/><br/>
 	  <br/><br/>
 	  <input type="hidden" value="-1" name="id" >
 	  <button type="submit">Cadastrar</button>
